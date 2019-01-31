acb73ba110a7cf6b245bfe9f30f288323a2ef88a
Amine42 <amine.bouraghda@gmail.com>
2019-01-31 04:58:14 +0100

    SA MARCHE

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,19 +2,19 @@ require 'faker' #on fait un require de la gem faker
 
 # on fait 10 touts de boucles
 10.times do
-    # on remplis la table user avec des donnee cree par faker
-    user = User.create!(name: Faker::Name.name)
+  # on remplis la table user avec des donnee cree par faker
+  user = User.create!(name: Faker::Name.name)
 end
 
 # on fait 10 touts de boucles
 10.times do
-    # on remplis la table pin avec des donnee cree par faker
-    pin = Pin.create!(user_id: rand(1..9),url_image: Faker::Internet.url)
+  # on remplis la table pin avec des donnee cree par faker
+  pin = Pin.create!(user_id: rand(1..9),url_image: Faker::Internet.url)
 end
 
 # on fait 10 touts de boucles
 10.times do
-    # on remplis la table comment avec des donnee cree par faker
-    comment = Comment.create!(pin_id: rand(1..9),text: Faker::Superhero.descriptor,user_id: rand(1..9))
+  # on remplis la table comment avec des donnee cree par faker
+  comment = Comment.create!(pin_id: rand(1..9),text: Faker::Superhero.descriptor,user_id: rand(1..9))
 end
 
